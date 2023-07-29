b8d59045215a8f0861c50c75bc734c88ee66b83e
evyatarmichlis <evyatar.michlis@placer.ai>
2023-07-29 20:38:03 +0300

    rami_levi_resources.py

--- rami_levi_scrapper/rami_levi_scrapper.py
+++ rami_levi_scrapper/rami_levi_scrapper.py
@@ -3,25 +3,8 @@ import time
 import pandas as pd
 import requests
 
-import mysql.connector
 
-db_config_ = {
-    "host": "sql7.freesqldatabase.com",
-    "user": "sql7636093",
-    "database": "sql7636093",
-    "password": "yx6aDgSuzq"
-}
-
-def upload_df_to_mysql(dataframe, table_name, db_config=None):
-    if db_config is None:
-        db_config = db_config_
-
-        connection = mysql.connector.connect(**db_config)
-        dataframe.to_sql(name=table_name, con=connection, if_exists='replace', index=False)
-
-        print(f"DataFrame uploaded to table '{table_name}' in MySQL database successfully.")
-
-
+from mysql_handler import MySQL
 
 url = 'https://www.rami-levy.co.il/api/catalog?=null'
 headers = {
@@ -57,59 +40,8 @@ def fetch_all_products(store_id=331, limit=300):
     df["timestamp"] = time.time()
     df["store_id"] = store_id
     df = df.rename({"group":"category","id":"item_id","subGroup":"subgroup"})
-    upload_df_to_mysql(df,"items")
+
+    MySQL.insert_df(df,"items")
 
 fetch_all_products()
 
-# import pandas as pd
-# import requests
-#
-# url = 'https://www.rami-levy.co.il/api/catalog?=null'
-# headers = {
-#     'Content-Type': 'application/json',
-#     'Cookie': 'auth.strategy=local; i18n_redirected=he'
-# }
-#
-# def create_payload(d, store, from_):
-#     data = {
-#         'd': d,
-#         'store': store,
-#         'from': from_
-#     }
-#     return data
-#
-# def fetch_all_products(store_id=331, max_depth=300):
-#     all_products = []
-#     limit = 600
-#     init_limit = False
-#
-#     for d in range(0, max_depth):
-#         for f in range(0, limit, 30):
-#             response = requests.post(url, headers=headers, json=create_payload(d, store=store_id, from_=f))
-#
-#             if response.status_code == 200:
-#                 response_data = response.json()
-#
-#                 if not init_limit:
-#                     limit = response_data["total"]
-#                     init_limit = True
-#
-#                 data = response_data["data"]
-#                 all_products.append(data)
-#
-#     return all_products
-#
-# def main():
-#     store_id = 331
-#     max_depth = 300
-#
-#     all_products = fetch_all_products(store_id=store_id, max_depth=max_depth)
-#
-#     if all_products:
-#         df = pd.DataFrame(all_products)
-#         print(df)
-#     else:
-#         print("No products found!")
-#
-# if __name__ == "__main__":
-#     main()
